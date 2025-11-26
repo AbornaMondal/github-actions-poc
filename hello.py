d8a57087c1c96fbb7035ed5a0bdee4ba862ceb4a
AbornaMondal <78502625+AbornaMondal@users.noreply.github.com>
2025-11-26 15:16:53 +0530

    Add greeting and current time display in hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+import datetime
+
+print(f"Hello from GitHub Actions!")
+print(f"Current time is: {datetime.datetime.now()}")
+
+# Simple check to show the script ran successfully
+if __name__ == "__main__":
+    print("Script execution complete.")
